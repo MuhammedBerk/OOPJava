e3e4cb7c8546f14c32dda6bf54ef6473d7bd9164
Muhammed Berk ŞİŞLİ <59254446+MuhammedBerk@users.noreply.github.com>
2020-03-22 19:34:30 +0300

    Lab2Q2

---
+++ Student.java
@@ -0,0 +1,21 @@
+package Lab2.Q2;
+
+public class Student {
+
+        String name;
+        int age;
+        boolean isJunior;
+        char gender;
+
+        public Student(){
+            name = "";
+            age = 0;
+            isJunior = false;
+            gender = 'm';
+        }
+
+        public void info(){
+            System.out.println("Student: " + name + "\nAge: " + age + "\nGender: " + gender + "\nJunior: " + isJunior + "\n");
+        }
+    }
+
